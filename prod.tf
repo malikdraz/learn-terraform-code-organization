25f7002af83dc344aad72b7567dd43de8c07c731
Malik Draz <malikdraz@hotmail.com>
2022-12-27 18:34:06 +1100

    seperated prod and dev

--- prod.tf
+++ prod.tf
@@ -20,60 +20,6 @@ resource "random_pet" "petname" {
   separator = "-"
 }
 
-resource "aws_s3_bucket" "dev" {
-  bucket = "${var.dev_prefix}-${random_pet.petname.id}"
-
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_website_configuration" "dev" {
-  bucket = aws_s3_bucket.dev.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_bucket_acl" "dev" {
-  bucket = aws_s3_bucket.dev.id
-
-  acl = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "dev" {
-  bucket = aws_s3_bucket.dev.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.dev.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_s3_object" "dev" {
-  acl          = "public-read"
-  key          = "index.html"
-  bucket       = aws_s3_bucket.dev.id
-  content      = file("${path.module}/assets/index.html")
-  content_type = "text/html"
-}
-
 resource "aws_s3_bucket" "prod" {
   bucket = "${var.prod_prefix}-${random_pet.petname.id}"
 
@@ -95,7 +41,7 @@ resource "aws_s3_bucket_website_configuration" "prod" {
 resource "aws_s3_bucket_acl" "prod" {
   bucket = aws_s3_bucket.prod.id
 
-  acl = "public-read"
+  acl = "private"
 }
 
 resource "aws_s3_bucket_policy" "prod" {
@@ -121,7 +67,7 @@ EOF
 }
 
 resource "aws_s3_object" "prod" {
-  acl          = "public-read"
+  acl          = "private"
   key          = "index.html"
   bucket       = aws_s3_bucket.prod.id
   content      = file("${path.module}/assets/index.html")
